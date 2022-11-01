117c6b3723770e98861918f8f92a869f74296564
José Antonio Castañeda Pavón <92045509+Casta626@users.noreply.github.com>
2022-11-01 20:13:26 +0100

    Update index.blade.php

--- resources/views/rm_max/index.blade.php
+++ resources/views/rm_max/index.blade.php
@@ -23,7 +23,164 @@
     @section('content')
     <div class="inicio">
         <form method="get" action="{{ route('rmmax.rmcalculator') }}">
+            <div class="centrado">
 
+                <a href="/"><img src="{!! url('gymnotes.ico') !!}" alt="GymNotes logo"></a>
+            </div>
+
+            <h1>Descubre tu RM</h1>
+
+            {{-- @include('layouts.partials.messages') --}}
+
+            <div class="form-group form-floating mb-3">
+                <input type="text" class="form-control" name="peso" value="{{ old('peso') }}" placeholder="peso"
+                    required="required" autofocus>
+                <label for="floatingName">Introduce el peso</label>
+                @if ($errors->has('peso'))
+                <span class="text-danger text-left">{{ $errors->first('peso') }}</span>
+                @endif
+            </div>
+
+            <div class="form-group form-floating mb-3">
+                <input type="text" class="form-control" name="rep" value="{{ old('rep') }}" placeholder="rep"
+                    required="required">
+                <label for="floatingPassword">Introduce las repeticiones</label>
+                @if ($errors->has('rep'))
+                <span class="text-danger text-left">{{ $errors->first('rep') }}</span>
+                @endif
+            </div>
+
+            <button href="{{ route('rmmax.rmcalculator') }}" class="w-100 btn btn-lg btn-danger" type="submit">Descubre
+                tu RM</button>
+
+            {{-- @if (count($resultados) > 0 )
+            poner mas cajas de formulario.
+
+            @else
+
+            @endif --}}
+            {{-- <div>{{ $resultados[0] }}</div> --}}
+
+
+
+            {{-- @include('auth.partials.copy') --}}
+            
+        </form>
+
+        {{-- Cirulares --}}
+        <section class="progress-section">
+            <div class="container">
+                <div class="row">
+                    
+                    
+                    <div class="col-md-3">
+                        <div class="progress-bars">
+                            <h2>20 KG</h2>
+                        </div>
+                        <h2 style="margin-top: 40px;margin-left:2%">44%</h2>
+                        <!--progress-bars-->
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="progress-bars-four">
+                            <h2>40%</h2>
+                        </div>
+                        <!--progress-bars-->
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="progress-bars-six">
+                            <h2>65%</h2>
+                        </div>
+                        <!--progress-bars-->
+                    </div>
+
+
+                    <div class="col-md-3">
+                        <div class="progress-bars-seven">
+                            <h2>70%</h2>
+                        </div>
+                        <!--progress-bars-->
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    
+                    
+                    <div class="col-md-3">
+                        <div class="progress-bars">
+                            <h2>20 KG</h2>
+                        </div>
+                        <h2 style="margin-top: 40px;margin-left:2%">44%</h2>
+                        <!--progress-bars-->
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="progress-bars-four">
+                            <h2>40%</h2>
+                        </div>
+                        <!--progress-bars-->
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="progress-bars-six">
+                            <h2>65%</h2>
+                        </div>
+                        <!--progress-bars-->
+                    </div>
+
+
+                    <div class="col-md-3">
+                        <div class="progress-bars-seven">
+                            <h2>70%</h2>
+                        </div>
+                        <!--progress-bars-->
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    
+                    
+                    <div class="col-md-3">
+                        <div class="progress-bars">
+                            <h2>20 KG</h2>
+                        </div>
+                        <h2 style="margin-top: 40px;margin-left:2%">44%</h2>
+                        <!--progress-bars-->
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="progress-bars-four">
+                            <h2>40%</h2>
+                        </div>
+                        <!--progress-bars-->
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="progress-bars-six">
+                            <h2>65%</h2>
+                        </div>
+                        <!--progress-bars-->
+                    </div>
+
+
+                    <div class="col-md-3">
+                        <div class="progress-bars-seven">
+                            <h2>70%</h2>
+                        </div>
+                        <!--progress-bars-->
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        
+
+        {{-- * Barras de progreso
             <div class="progress">
 
                 <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="10" aria-valuemin="0"
@@ -77,90 +234,8 @@
                     <span class="sr-only">45% completado</span>
                 </div>
 
-            </div>
+            </div> --}}
 
-
-            {{-- Cirulares --}}
-            <section class="progress-section">
-                <div class="container">
-                    <div class="row">
-                        
-                        
-                        <div class="col-md-3">
-                            <div class="progress-bars">
-                                <h2>20 KG</h2>
-                            </div>
-                            <h2 style="margin-top: 40px;margin-left:2%">44%</h2>
-                            <!--progress-bars-->
-                        </div>
-
-                        <div class="col-md-3">
-                            <div class="progress-bars-four">
-                                <h2>40%</h2>
-                            </div>
-                            <!--progress-bars-->
-                        </div>
-
-                        <div class="col-md-3">
-                            <div class="progress-bars-six">
-                                <h2>65%</h2>
-                            </div>
-                            <!--progress-bars-->
-                        </div>
-
-
-                        <div class="col-md-3">
-                            <div class="progress-bars-seven">
-                                <h2>70%</h2>
-                            </div>
-                            <!--progress-bars-->
-                        </div>
-                    </div>
-                </div>
-            </section>
-
-            <div class="centrado">
-
-                <a href="/"><img src="{!! url('gymnotes.ico') !!}" alt="GymNotes logo"></a>
-            </div>
-
-            <h1>Descubre tu RM</h1>
-
-            {{-- @include('layouts.partials.messages') --}}
-
-            <div class="form-group form-floating mb-3">
-                <input type="text" class="form-control" name="peso" value="{{ old('peso') }}" placeholder="peso"
-                    required="required" autofocus>
-                <label for="floatingName">Introduce el peso</label>
-                @if ($errors->has('peso'))
-                <span class="text-danger text-left">{{ $errors->first('peso') }}</span>
-                @endif
-            </div>
-
-            <div class="form-group form-floating mb-3">
-                <input type="text" class="form-control" name="rep" value="{{ old('rep') }}" placeholder="rep"
-                    required="required">
-                <label for="floatingPassword">Introduce las repeticiones</label>
-                @if ($errors->has('rep'))
-                <span class="text-danger text-left">{{ $errors->first('rep') }}</span>
-                @endif
-            </div>
-
-            <button href="{{ route('rmmax.rmcalculator') }}" class="w-100 btn btn-lg btn-danger" type="submit">Descubre
-                tu RM</button>
-
-            {{-- @if (count($resultados) > 0 )
-            poner mas cajas de formulario.
-
-            @else
-
-            @endif --}}
-            {{-- <div>{{ $resultados[0] }}</div> --}}
-
-
-
-            {{-- @include('auth.partials.copy') --}}
-        </form>
     </div>
     @endsection
 </body>
